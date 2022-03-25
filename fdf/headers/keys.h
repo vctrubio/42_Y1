498da6cf0bad3c1464eb42da506ec2d867aca571
vRubio <vctrubio@gmail.com>
2022-03-25 11:34:01 +0000

    fdf

---
+++ fdf/headers/keys.h
@@ -0,0 +1,37 @@
+#ifndef KEYS_H
+# define KEYS_H
+
+# define K_A 0
+# define K_B 11
+# define K_C 8
+# define K_D 2
+# define K_E 14
+# define K_F 3
+# define K_G 5
+# define K_H 4
+# define K_I 34
+# define K_J 38
+# define K_K 40
+# define K_L 37
+# define K_M 46
+# define K_N 45
+# define K_O 31
+# define K_P 35
+# define K_Q 12
+# define K_R 15
+# define K_S 1
+# define K_T 17
+# define K_U 32
+# define K_V 9
+# define K_W 13
+# define K_X 7
+# define K_Y 16
+# define K_Z 6
+
+# define K_LEFT 123
+# define K_RIGHT 124
+# define K_UP 126
+# define K_DOWN 125
+
+# define K_ESC 53
+#endif
